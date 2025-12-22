6347b068c65680376c8b0aa74f711bdba45138c4
@Miguel_Rios <miguel.rios0724@jala.university>
2025-12-22 10:20:33 -0500

    Git init, First proyect of SpringBoot API created

---
+++ ApiOfProducts/src/main/java/MiguelRios/Kandelhari/ApiOfProducts/ApiOfProductsApplication.java
@@ -0,0 +1,13 @@
+package MiguelRios.Kandelhari.ApiOfProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOfProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOfProductsApplication.class, args);
+	}
+
+}
---
+++ ApiOfProducts/src/test/java/MiguelRios/Kandelhari/ApiOfProducts/ApiOfProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package MiguelRios.Kandelhari.ApiOfProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOfProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
